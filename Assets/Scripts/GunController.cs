0b3387e3a15afeebb0d1b2f56f752d7e167a9881
Nadav Ben-Amram <nadavbenamram@gmail.com>
2017-11-04 19:45:13 +0200

    m4a1 rifle + sound

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZEffects;
+
+public class GunController : MonoBehaviour {
+
+	public GameObject m_GunShot;
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+
+	private bool m_LastTriggerDown = false;
+	void Update() {
+		bool currentTrigger = GvrPointerInputModule.Pointer.TriggerDown;
+		if (true == currentTrigger && false == m_LastTriggerDown) {
+			GameObject gunShot = Instantiate (m_GunShot, this.transform.position, this.transform.rotation) as GameObject;
+		}
+
+		m_LastTriggerDown = currentTrigger;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GVR;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GvrControllerInput.ClickButton) {
+			Debug.Log ("test123");
+		}
+	}
+
+	public void GunShot()
+	{
+		Debug.Log ("Gun SHOOT!!!");
+	}
+
+	public void test()
+	{
+		Debug.Log ("Gun SHOOT!!!");
+	}
+}
