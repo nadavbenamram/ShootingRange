0bd9867fa8e6fbf6f0bca1e0ffb90838fc075b91
Nadav Ben-Amram <nadavbenamram@gmail.com>
2017-11-07 21:32:48 +0200

    Fireball and Weapons manager

---
+++ Assets/Scripts/FireballController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballController : MonoBehaviour
+{
+	GameObject m_FireBallPrefab;
+	// Use this for initialization
+	void Start ()
+	{
+		m_FireBallPrefab = Resources.Load ("FireBall") as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (GvrPointerInputModule.Pointer.TriggerDown) 
+		{
+			GameObject fireBall = Instantiate(m_FireBallPrefab, this.transform.position + new Vector3(0, 1), this.transform.rotation) as GameObject;
+			Rigidbody rigidbody = fireBall.GetComponent<Rigidbody> ();
+			rigidbody.velocity = this.transform.forward * 15;
+		}
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,27 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 using GVR;
 
-public class PlayerController : MonoBehaviour {
+public class WeaponsManager
+{
+	private const int NUM_OF_WEAPONS = 3;
+	private const string GUN_KEY = "gun";
+	private const string LASER_KEY = "laser";
+	private const string FIREBALL_KEY = "fireball";
+	private const string DEFAULT_WEAPON_KEY = GUN_KEY; //Gun is the default weapon;
+	private const string GUN_PREFAB_NAME = "Gun";
+	private const string LASER_PREFAB_NAME = "Laser";
+	private const string FIREBALL_PREFAB_NAME = "Fireball";
+	private Dictionary<string, GameObject> m_Weapons;
+	private GameObject m_ActiveWeapon;
 
-	// Use this for initialization
-	void Start () {
-		
+	public WeaponsManager()
+	{
+		init ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (GvrControllerInput.ClickButton) {
-			Debug.Log ("test123");
+
+	private void init()
+	{
+		m_Weapons = new Dictionary<string, GameObject> (NUM_OF_WEAPONS);
+		m_Weapons [GUN_KEY] = GameObject.Find (GUN_PREFAB_NAME);
+		m_Weapons [LASER_KEY] = GameObject.Find (LASER_PREFAB_NAME);
+		m_Weapons [FIREBALL_KEY] = GameObject.Find (FIREBALL_PREFAB_NAME);
+
+		//Inactive all the weapons
+		foreach (GameObject gameObject in m_Weapons.Values) 
+		{
+			gameObject.SetActive (false);
 		}
+
+		//set default weapon active
+		m_ActiveWeapon = m_Weapons [DEFAULT_WEAPON_KEY];
+		m_ActiveWeapon.SetActive (true);
 	}
 
-	public void GunShot()
+	public void SwitchWeapon(string i_WeaponKey)
 	{
-		Debug.Log ("Gun SHOOT!!!");
+		m_ActiveWeapon.SetActive (false);
+		m_ActiveWeapon = m_Weapons [i_WeaponKey];
+		m_ActiveWeapon.SetActive (true);
+	}
+}
+
+public class PlayerController : MonoBehaviour 
+{
+	private WeaponsManager m_WeaponManager;
+	// Use this for initialization
+	void Start () 
+	{
+		m_WeaponManager = new WeaponsManager ();
 	}
 
-	public void test()
-	{
-		Debug.Log ("Gun SHOOT!!!");
+	string[] test = { "laser", "fireball", "gun" }; 
+	int i = 0;
+	// Update is called once per frame
+	bool wasdown = false;
+	void Update () {
+		bool down = Input.GetMouseButton(1);
+		if (down && !wasdown) 
+		{
+			m_WeaponManager.SwitchWeapon (test[i%3]);
+			i++;
+		}
+
+		wasdown = down;
 	}
 }
